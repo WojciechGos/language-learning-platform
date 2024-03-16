abec67d0d3cfa49d5317938c46b7959bedd12e71
fafusinho <krzysiek10911@gmail.com>
2024-03-16 23:30:24 +0100

    Classes level indicator

--- frontend/src/pages/Classes.tsx
+++ frontend/src/pages/Classes.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Navigation from "../components/Navigation";
+import MessageBoard from "../components/MessageBoard";
+import GroupTile from "../components/GroupTile";
 
 const Classes: React.FC = () => {
   return (
     <div>
       <Navigation />
       <h1>Lekcje</h1>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <MessageBoard />
+        <GroupTile name="Twój poziom" level={"B1"} />
+      </div>
     </div>
   );
 };
